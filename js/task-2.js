10cc6fcb14e45b34af56387fe97794a7ae340c1f
Nelia Demenska <demenskaya@nelya.gmail.com>
2024-07-17 16:40:57 +0200

    tasks

---
+++ js/task-2.js
@@ -0,0 +1,15 @@
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        const expression = message.slice();
+        return expression;
+    } else {
+        const answer = `${message.slice(0, maxLength)}...`;
+        return answer;
+    }
+}
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
